f596576c1526e9b083a41c639f82777c5b59ff98
mershter <100219272+mershter@users.noreply.github.com>
2023-01-19 08:41:50 -0500

    troubleshooted everything with Daniel
    - everything should work, needs adjustments

--- scripts.js
+++ scripts.js
@@ -4,20 +4,20 @@ $('.hamburger-button').click(function(){
     $(this).toggleClass('active')
 });
 
-let slideIndex = 0;
-showSlides();
+// let slideIndex = 0;
+// showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000); // Change image every 2 seconds
-} 
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 4000); // Change image every 2 seconds
+// } 
 
 AOS.init({
   duration: 2500 // values from 0 to 3000, with step 50ms
